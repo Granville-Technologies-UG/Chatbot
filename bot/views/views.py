409e445761375d1190ac9e729bd44bb9a6880c42
Martin Kalema <kalemaaaaaaaa@gmail.com>
2024-08-05 14:15:35 +0300

    chore: Test Chat Completions API

--- bot/views/views.py
+++ bot/views/views.py
@@ -55,7 +55,7 @@ def verify():
     mode = request.args.get("hub.mode")
     token = request.args.get("hub.verify_token")
     challenge = request.args.get("hub.challenge")
-    
+
     if mode and token:
         if mode == "subscribe" and token == current_app.config["VERIFY_TOKEN"]:
             logging.info("WEBHOOK_VERIFIED")
@@ -73,9 +73,8 @@ def verify():
 def webhook_get():
     return verify()
 
+
 @webhook_blueprint.route("/webhook", methods=["POST"])
 @signature_required
 def webhook_post():
     return handle_message()
-
-
